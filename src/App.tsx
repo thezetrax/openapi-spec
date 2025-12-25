b02331fbc3bd2d168d0eb1b7414e6b9060676871
Zablon Dawit <zabjd22@gmail.com>
2025-12-25 15:21:27 +0300

    feat: add scalar api reference UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ApiReferenceReact } from "@scalar/api-reference-react";
+import "@scalar/api-reference-react/style.css";
+
 function App() {
-  return <>Hello, World!</>;
+  return (
+    <ApiReferenceReact
+      configuration={{
+        url: "https://registry.scalar.com/@scalar/apis/galaxy?format=yaml",
+      }}
+    />
+  );
 }
 
 export default App;
